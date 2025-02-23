b02d73c331d68ecebed65be57ce78aa65eaa47e5
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 01:37:44 +0300

    Unify plugins deployment

--- Plugin.Caching.Web/Properties/AssemblyInfo.cs
+++ Plugin.Caching.Web/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("f4664df2-87f3-4bb6-a271-ff0da4d4fa2a")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/Plugin.Caching.Web")]
 #else
 
-[assembly: AssemblyTitle("Plugin.Caching.Web")]
 [assembly: AssemblyDescription("System.Web.Caching.Cache is used for caching")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.Caching.Web")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
 #endif
